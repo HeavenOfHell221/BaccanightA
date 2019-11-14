7899d59d801bdecd3d167bc3462fc2c775fb5e04
Gabriel <dubois.gabriel@hotmail.com>
2019-11-14 17:14:28 +0100

    Ajout du projet test d'Adrien
    3 pages html + css + js

---
+++ script/script.js
@@ -0,0 +1,33 @@
+function toggleVisibility(ID){
+    var cible = document.getElementById(ID);
+    
+        cible.style.display = "block";
+}
+
+function toggleHidden(ID){
+    var cible = document.getElementById(ID);
+    cible.style.display = "none";
+}
+
+
+function toggleBLOCK(ID){
+    var cible = document.getElementById(ID);    
+    cible.style.display = "block";        
+}
+
+function toggleNONE(ID){
+    var cible = document.getElementById(ID);    
+    cible.style.display = "none";        
+}
+
+function toggleON(ID){
+    var cible = document.getElementById(ID);    
+    cible.style.left = "0vw";      
+}
+
+function toggleOFF(ID){
+    var cible = document.getElementById(ID);    
+    cible.style.left = "-100vw";      
+}
+
+
