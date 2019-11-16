c1a4da7b109c3d9ec02a6330c63103726e04a7e3
Popof <jeanchristopheblin1@hotmail.fr>
2019-11-16 16:34:37 +0100

    Ajout canvas dessin
    Ajout du canvas de dessin, reste encore à trouver une valeur précise pour l'écart avec le haut de l'écran pour que le canvas prenne tout l'écran

---
+++ script/canvas.js
@@ -0,0 +1,49 @@
+window.addEventListener('load', () => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    const topMargin = 0.05*window.innerHeight;
+    var clientX;
+    var clientY;
+
+    //Resizing
+    canvas.height = window.innerHeight - topMargin;
+    canvas.width = window.innerWidth;
+    ctx.lineWidth = 10;      // taille tracé
+    ctx.lineCap = "round";   // Forme tracé
+    ctx.strokeStyle = "black"; // Couleur
+
+    //variables
+    let painting = false;
+
+    function startPosition(e){
+        painting = true;
+        draw(e);
+    }
+    function finishedPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+    function draw(e){
+        clientX = e.touches[0].clientX;
+        clientY = e.touches[0].clientY - topMargin;
+        if(!painting) return;
+
+        ctx.lineTo(clientX, clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(clientX, clientY);
+    }
+    
+    //EventListenners
+    /* POUR NAVIGATEUR WEB
+     * canvas.addEventListener('mousedown',startPosition);
+     * canvas.addEventListener('mouseup',finishedPosition);
+     * canvas.addEventListener('mousemove',draw);
+     */
+    /* POUR NAVIGATEUR MOBILE 
+     */
+    canvas.addEventListener('touchstart',startPosition);
+    canvas.addEventListener('touchend',finishedPosition);
+    canvas.addEventListener('touchmove',draw);
+});
