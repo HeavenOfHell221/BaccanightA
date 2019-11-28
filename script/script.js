3fd9a2cc154c2a27249aed4aad28db18231b7fb4
Gabriel <dubois.gabriel@hotmail.com>
2019-11-28 14:13:31 +0100

    Modif d'Adrien

--- script/script.js
+++ script/script.js
@@ -1,23 +1,11 @@
-function toggleVisibility(ID){
-    var cible = document.getElementById(ID);
-    
-        cible.style.display = "block";
-}
-
-function toggleHidden(ID){
-    var cible = document.getElementById(ID);
-    cible.style.display = "none";
-}
-
-
 function toggleBLOCK(ID){
     var cible = document.getElementById(ID);    
-    cible.style.display = "block";        
+    cible.style.display = "block";
 }
 
 function toggleNONE(ID){
     var cible = document.getElementById(ID);    
-    cible.style.display = "none";        
+    cible.style.display = "none";
 }
 
 function toggleON(ID){
