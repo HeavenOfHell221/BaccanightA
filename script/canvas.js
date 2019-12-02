ec977ccd7031aeed076502dd2e54b111919e196a
Popof <jeanchristopheblin1@hotmail.fr>
2019-12-02 16:51:26 +0100

    Amelioration interface de dessin
    Ajout d'une slidebar pour la taille, la gomme qui s'adapte avec le fond, reste encore à faire une selection de couleurs avec l'outil couleur et l'outil fond.
    Aussi l'outil retour je ne sais pas pour le moment comment faire

--- script/canvas.js
+++ script/canvas.js
@@ -1,27 +1,42 @@
 window.addEventListener('load', () => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-
+    
     const topMargin = 0.25*window.innerHeight;
     var clientX;
     var clientY;
+    var penColor = "black"
+    var backgroundColor = "white";
+
+    var buttonPen = document.getElementById("buttonPen");
+    var buttonColor = document.getElementById("buttonColor");
+    var buttonRubber = document.getElementById("buttonRubber");
+    var buttonSize = document.getElementById("buttonSize");
+    var buttonReturn = document.getElementById("buttonReturn");
+    var buttonPaint = document.getElementById("buttonPaint");
+    var sizeRange = document.getElementById("sizeRange");
+    var outputRange = document.getElementById("displayDrawSize");
+    
 
     //Resizing
     canvas.height = window.innerWidth;
     canvas.width = window.innerWidth;
     ctx.lineWidth = 10;      // taille tracé
     ctx.lineCap = "round";   // Forme tracé
-    ctx.strokeStyle = "black"; // Couleur
+    ctx.strokeStyle = penColor; // Couleur
+
+    outputRange.style.width = ctx.lineWidth; //Affiche la valeur par défaut du slider
+    outputRange.style.height = ctx.lineWidth;
 
     //variables
-    let painting = false;
+    let painting = true;
 
     function startPosition(e){
-        painting = true;
+        //painting = true;
         draw(e);
     }
     function finishedPosition(){
-        painting = false;
+        //painting = false;
         ctx.beginPath();
     }
     function draw(e){
@@ -33,7 +48,41 @@ window.addEventListener('load', () => {
         ctx.stroke();
         ctx.beginPath();
         ctx.moveTo(clientX, clientY);
-        
+    }
+
+    /************** BOUTONS **************/
+    buttonPen.onclick = function() {
+        sizeRange.style.display="none";
+        ctx.strokeStyle = penColor;
+    }
+    buttonColor.onclick = function() {
+        sizeRange.style.display="none";
+    }
+    buttonRubber.onclick = function() {
+        sizeRange.style.display="none";
+        ctx.strokeStyle = backgroundColor;
+    }
+    /*** Slider taille ***/
+    buttonSize.onclick = function() {
+        sizeRange.style.display="initial";
+    }
+
+    buttonReturn.onclick = function() {
+        sizeRange.style.display="none";
+    }
+    buttonPaint.onclick = function() {
+        sizeRange.style.display="none";
+        if(backgroundColor == "red")
+        backgroundColor = "white";
+        else
+        backgroundColor = "red";
+        canvas.style.background = backgroundColor;
+    }
+
+    sizeRange.oninput = function() {
+        ctx.lineWidth = this.value;
+        outputRange.style.width = ctx.lineWidth+"px"; //Affiche la valeur par défaut du slider
+        outputRange.style.height = ctx.lineWidth+"px";
     }
     //canvas.style.background = "red";
     
