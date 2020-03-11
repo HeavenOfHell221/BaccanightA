33af61377204647f742fc644c543ab820037354c
Gabriel <dubois.gabriel@hotmail.com>
2020-03-11 11:48:11 +0100

    Fix bug TotalConfiner

--- Baccanight_Unity/Assets/Scripts/Utility/ConfinerLayout.cs
+++ Baccanight_Unity/Assets/Scripts/Utility/ConfinerLayout.cs
@@ -1,22 +1,50 @@
 ﻿using System.Collections;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ConfinerLayout : MonoBehaviour
 {
-	static GameObject totalConfiner;
 
-	void Start()
+    #region Inspector
+    [SerializeField]
+    private int m_ID = -1;
+    #endregion
+
+    #region Variables
+    private static GameObject m_totalConfiner;
+    private static Dictionary<int, bool> m_IDDict = new Dictionary<int, bool>(); 
+    #endregion
+
+
+    void Start()
 	{
-		StartCoroutine(GetTotalConfiner());
+        if(m_ID == -1)
+        {
+            Debug.LogError("ConfinerLayout ID ERROR. (-1)");
+        }
+        else
+        {
+            if(m_IDDict.ContainsKey(m_ID))
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                m_IDDict.Add(m_ID, true);
+                StartCoroutine(GetTotalConfiner());
+            }            
+        }
 	}
 
 	IEnumerator GetTotalConfiner()
 	{
-		while (totalConfiner == null)
+		while (m_totalConfiner == null)
 		{
-			totalConfiner = GameObject.Find("TotalConfiner");
+			m_totalConfiner = GameObject.Find("TotalConfiner");
 			yield return null;
 		}
-		gameObject.transform.SetParent(totalConfiner.transform);
+		gameObject.transform.SetParent(m_totalConfiner.transform);
 	}
+
+    
 }
