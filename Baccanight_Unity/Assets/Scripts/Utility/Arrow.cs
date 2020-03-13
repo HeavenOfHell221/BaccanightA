4106202f2b7053172c5b9ed696ed33d5eb76a316
Popof <jeanchristopheblin1@hotmail.fr>
2020-03-13 17:07:52 +0100

    Scripts flèches (juste les scripts donc pas fini))

---
+++ Baccanight_Unity/Assets/Scripts/Utility/Arrow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private float ArrowSpeed = 20f;
+
+	[SerializeField]
+	private LayerMask whatIsGround;
+	[SerializeField]
+	private LayerMask whatIsBoss;
+
+	private LayerMask HitMask;
+
+	[SerializeField]
+	private Transform m_HitCheck;
+
+	void Start()
+    {
+		HitMask = whatIsBoss | whatIsGround;
+    }
+
+    void FixedUpdate()
+    {
+		transform.position = transform.forward * ArrowSpeed;
+		CheckHit();
+    }
+
+	protected void CheckHit()
+	{
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_HitCheck.position, .05f, HitMask);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				if (colliders[i].gameObject.layer == whatIsBoss)
+				{
+					//Deal Damage
+				}
+				Destroy(gameObject);
+			}
+		}
+	}
+}
