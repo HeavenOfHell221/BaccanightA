cf704e2e813b2a55c608b5e3384602ec9845c0f9
Gabriel <dubois.gabriel@hotmail.com>
2020-03-28 10:22:05 +0100

    Début dialogue tableau

---
+++ Baccanight_Unity/Assets/Scripts/DialogBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogBox : MonoBehaviour
+{
+    public void ActiveDialog()
+    {
+
+    }
+
+    public void CancelDialog()
+    {
+
+    }
+}
---
+++ Baccanight_Unity/Assets/Scripts/PaintingDialog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintingDialog : MonoBehaviour
+{
+    private DialogBox m_dialogBox;
+
+    private void Start()
+    {
+        GameObject box = GameObject.FindGameObjectWithTag("DialogBox");
+        if(box)
+        {
+            m_dialogBox = box.GetComponent<DialogBox>();
+        }
+    }
+
+
+    public void EnterPlayer(GameObject player)
+    {
+
+    }
+}
