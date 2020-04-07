f39ae681f25e05b7f07414af043ed896b4de4f30
Gabriel <dubois.gabriel@hotmail.com>
2020-04-07 15:55:20 +0200

    Light map 1.1 et 1.2 et 2.0

---
+++ Baccanight_Unity/Assets/Piedestal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piedestal : MonoBehaviour
+{
+    public void CollisionEnter(GameObject obj)
+    {
+        if(obj.tag == "Statue")
+        {
+            Debug.Log("Statue !");
+        }
+    }
+}
