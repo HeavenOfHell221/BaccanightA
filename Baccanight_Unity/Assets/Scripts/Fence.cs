db104eb5647e39d4c5aefe2d1473cbaefcca9402
Quentin <quentinfergelot@gmail.com>
2020-04-12 11:56:29 +0200

    La même avec un lerp

--- Baccanight_Unity/Assets/Scripts/Fence.cs
+++ Baccanight_Unity/Assets/Scripts/Fence.cs
@@ -6,6 +6,7 @@ public class Fence : MonoBehaviour
 {
     [SerializeField] private bool m_isOpen = false;
     [SerializeField] private Collider2D m_collider;
+    private Vector3 m_endMarker;
 
     public void Start()
     {
@@ -15,7 +16,18 @@ public class Fence : MonoBehaviour
     public void OpenFence()
     {
         //m_collider.enabled = false;
-        transform.position += Vector3.up * 3;
+        m_endMarker = transform.position + Vector3.up * 3;
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        transform.position = Vector3.Lerp(transform.position, m_endMarker, 0.05f);
+        if (transform.position != m_endMarker)
+        {
+            StartCoroutine(Move());
+        }
+        else yield return null;
     }
 
     public void CloseFence()
